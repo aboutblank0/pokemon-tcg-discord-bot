95538b72cb8eaf3a9cfbe55a574b018b75c6abc3
aboutBlank-dev <josecolaco1999@gmail.com>
2025-03-21 00:30:45 +0000

    remove unused code

--- database/models/card_drop_event_model.py
+++ database/models/card_drop_event_model.py
@@ -1,6 +1,5 @@
 from sqlalchemy import ARRAY, BigInteger, Column, DateTime, ForeignKey, String, func
 from database.models.base import Base
-from drops.card_drop_event import CardDropEvent
 
 class CardDropEventModel(Base):
     __tablename__ = "card_drop_events"
@@ -10,11 +9,3 @@ class CardDropEventModel(Base):
     id = Column(String, primary_key=True, unique=True)
     cards_dropped_ids = Column(ARRAY(String), nullable=False)
     started_by_user_id = Column(BigInteger, ForeignKey("users.discord_user_id"), nullable=False)
-
-    @classmethod
-    def from_card_drop_event(cls, card_drop_event: CardDropEvent):
-        return cls(
-            id=card_drop_event.id,
-            cards_dropped_ids=[card.tcg_card.id for card in card_drop_event.dropped_cards],
-            started_by_user_id = card_drop_event.owner_discord_id
-        )
